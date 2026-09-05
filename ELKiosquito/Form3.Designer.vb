8aede6c9311f51286d1535c58f2e05e8a3c4b8e8
Ramiro Núñez <ramirosebastiann@gmail.com>
2026-09-05 16:38:57 -0300

    Se diseñó la vista del Cierre De Caja de Turno

---
+++ ELKiosquito/Form3.Designer.vb
@@ -0,0 +1,374 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Panel1 = New Panel()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Panel2 = New Panel()
+        Label6 = New Label()
+        Label7 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label8 = New Label()
+        TextBox3 = New TextBox()
+        Label9 = New Label()
+        TextBox4 = New TextBox()
+        Label10 = New Label()
+        TextBox5 = New TextBox()
+        Label11 = New Label()
+        TextBox6 = New TextBox()
+        Label12 = New Label()
+        TextBox7 = New TextBox()
+        Label13 = New Label()
+        Panel3 = New Panel()
+        Label14 = New Label()
+        Label15 = New Label()
+        Button1 = New Button()
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        Panel3.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 20F)
+        Label1.Location = New Point(12, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(183, 37)
+        Label1.TabIndex = 0
+        Label1.Text = "Cierre de Caja"
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = SystemColors.ActiveBorder
+        Panel1.Controls.Add(Label5)
+        Panel1.Controls.Add(Label4)
+        Panel1.Controls.Add(Label3)
+        Panel1.Controls.Add(Label2)
+        Panel1.Location = New Point(12, 64)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(776, 43)
+        Panel1.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label2.Location = New Point(13, 14)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(81, 15)
+        Label2.TabIndex = 0
+        Label2.Text = "Turno Activo:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label3.Location = New Point(116, 14)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(95, 15)
+        Label3.TabIndex = 1
+        Label3.Text = "00:00 - 08:00 hs"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label4.Location = New Point(512, 14)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(108, 15)
+        Label4.TabIndex = 2
+        Label4.Text = "Vendedor a Cargo:"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label5.Location = New Point(626, 14)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(93, 15)
+        Label5.TabIndex = 3
+        Label5.Text = "Carlos Gimenez"
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = SystemColors.ActiveCaption
+        Panel2.BorderStyle = BorderStyle.FixedSingle
+        Panel2.Controls.Add(Button1)
+        Panel2.Controls.Add(Panel3)
+        Panel2.Controls.Add(TextBox7)
+        Panel2.Controls.Add(Label13)
+        Panel2.Controls.Add(TextBox6)
+        Panel2.Controls.Add(Label12)
+        Panel2.Controls.Add(TextBox5)
+        Panel2.Controls.Add(Label11)
+        Panel2.Controls.Add(TextBox4)
+        Panel2.Controls.Add(Label10)
+        Panel2.Controls.Add(TextBox3)
+        Panel2.Controls.Add(Label9)
+        Panel2.Controls.Add(TextBox2)
+        Panel2.Controls.Add(Label8)
+        Panel2.Controls.Add(TextBox1)
+        Panel2.Controls.Add(Label7)
+        Panel2.Controls.Add(Label6)
+        Panel2.Location = New Point(12, 121)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(776, 317)
+        Panel2.TabIndex = 2
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Label6.ForeColor = SystemColors.ControlText
+        Label6.Location = New Point(310, 9)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(114, 21)
+        Label6.TabIndex = 0
+        Label6.Text = "Arqueo Ciego"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
+        Label7.Location = New Point(109, 43)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(147, 20)
+        Label7.TabIndex = 1
+        Label7.Text = "Billetes de $20.000:"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BorderStyle = BorderStyle.FixedSingle
+        TextBox1.Location = New Point(109, 66)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(142, 23)
+        TextBox1.TabIndex = 2
+        TextBox1.Text = "0"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BorderStyle = BorderStyle.FixedSingle
+        TextBox2.Location = New Point(107, 134)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(142, 23)
+        TextBox2.TabIndex = 4
+        TextBox2.Text = "0"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
+        Label8.Location = New Point(107, 111)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(147, 20)
+        Label8.TabIndex = 3
+        Label8.Text = "Billetes de $10.000:"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.BorderStyle = BorderStyle.FixedSingle
+        TextBox3.Location = New Point(303, 66)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(142, 23)
+        TextBox3.TabIndex = 6
+        TextBox3.Text = "0"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
+        Label9.Location = New Point(303, 43)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(138, 20)
+        Label9.TabIndex = 5
+        Label9.Text = "Billetes de $2.000:"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.BorderStyle = BorderStyle.FixedSingle
+        TextBox4.Location = New Point(303, 134)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(142, 23)
+        TextBox4.TabIndex = 8
+        TextBox4.Text = "0"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
+        Label10.Location = New Point(303, 111)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(138, 20)
+        Label10.TabIndex = 7
+        Label10.Text = "Billetes de $1.000:"
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.BorderStyle = BorderStyle.FixedSingle
+        TextBox5.Location = New Point(477, 66)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(142, 23)
+        TextBox5.TabIndex = 10
+        TextBox5.Text = "0"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
+        Label11.Location = New Point(477, 43)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(125, 20)
+        Label11.TabIndex = 9
+        Label11.Text = "Billetes de $500:"
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.BorderStyle = BorderStyle.FixedSingle
+        TextBox6.Location = New Point(477, 134)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(142, 23)
+        TextBox6.TabIndex = 12
+        TextBox6.Text = "0"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
+        Label12.Location = New Point(477, 111)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(125, 20)
+        Label12.TabIndex = 11
+        Label12.Text = "Billetes de $200:"
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.BorderStyle = BorderStyle.FixedSingle
+        TextBox7.Location = New Point(303, 198)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(142, 23)
+        TextBox7.TabIndex = 14
+        TextBox7.Text = "$$$$$"
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
+        Label13.Location = New Point(230, 175)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(322, 20)
+        Label13.TabIndex = 13
+        Label13.Text = "Total en pesos de billetes de $100 y menores"
+        ' 
+        ' Panel3
+        ' 
+        Panel3.BackColor = SystemColors.InactiveCaption
+        Panel3.Controls.Add(Label15)
+        Panel3.Controls.Add(Label14)
+        Panel3.Location = New Point(12, 246)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(473, 56)
+        Panel3.TabIndex = 15
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
+        Label14.Location = New Point(13, 19)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(257, 20)
+        Label14.TabIndex = 2
+        Label14.Text = "TOTAL EFECTIVO FÍSICO CONTADO:"
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
+        Label15.Location = New Point(286, 19)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(54, 20)
+        Label15.TabIndex = 3
+        Label15.Text = "$$$$$"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Button1.Location = New Point(491, 246)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(269, 56)
+        Button1.TabIndex = 16
+        Button1.Text = "Procesar Cierre de Turno Y Generar Ticket"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form3
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Panel2)
+        Controls.Add(Panel1)
+        Controls.Add(Label1)
+        Name = "Form3"
+        Text = "Form3"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        Panel3.ResumeLayout(False)
+        Panel3.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label14 As Label
+End Class
---
+++ ELKiosquito/Form3.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form3
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label13_Click(sender As Object, e As EventArgs) Handles Label13.Click
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub TextBox6_TextChanged(sender As Object, e As EventArgs) Handles TextBox6.TextChanged
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+End Class
